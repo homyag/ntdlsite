6fb4beeb3a216d63b828ebfa45221c7d2df2ee23
Igor Antonov <homyaga@gmail.com>
2024-10-17 15:25:46 +0300

    зарегистрированы урлы апи, изменен урл нерудки на главной

--- new_tdl_site/urls.py
+++ new_tdl_site/urls.py
@@ -7,6 +7,14 @@ from django.views.generic.base import TemplateView
 
 from new_tdl_site.views import page_not_found
 
+from leads.views import (CallsApiList, \
+                         CallsApiUpdate, CallsByManagerTgIdApiList,
+                         CallsByManagerTgIdApiUpdate,
+                         CallByManagerTgIdAndDateRangeApiList,
+                         CallCommentUpdateView, ManagersApiList,
+                         CallsDetailView, CallResultUpdateView, ResultsApiList,
+                         CallManagerUpdateView)
+
 from .sitemap import StaticViewSitemap, ProductSitemap, CategorySitemap
 
 sitemaps = {
@@ -22,6 +30,30 @@ admin.site.site_title = "Торговый Дом Ленинградский"
 
 urlpatterns = [
     path('tdladmin/', admin.site.urls),
+    path('api/v1/callslist/', CallsApiList.as_view()), # получает весь
+    # список заявок по GET запросу и добавляет новую заявку по POST запросу
+    path('api/v1/managers/', ManagersApiList.as_view()), # получает весь
+    # список менеджеров по GET запросу и добавляет нового менеджера по POST
+    # запросу
+    path('api/v1/results/', ResultsApiList.as_view()), # получает весь
+    # список результатов по GET запросу и добавляет новые резудьтаты по POST
+    path('api/v1/callslist/<int:pk>/', CallsApiUpdate.as_view()),
+    # обновляет одну заявку
+    path('api/v1/callslist/call/<int:pk>/', CallsDetailView.as_view()),
+    # получает одну заявку по id заявки
+    path('api/v1/callslist/manager-tg/<int:tg_id>/',
+         CallsByManagerTgIdApiList.as_view()), # получает список заявок по
+    # telegram id менеджера
+    path('api/v1/callslist/manager-tg/<int:tg_id>/<int:pk>/',
+         CallsByManagerTgIdApiUpdate.as_view()), # обновляет заявку по id менеджера и id заявки
+    path('api/v1/callslist/filter/',
+         CallByManagerTgIdAndDateRangeApiList.as_view()),
+    path('api/v1/callslist/comment_update/<int:pk>/',
+         CallCommentUpdateView.as_view()), # обновляет комментарий
+    path('api/v1/callslist/result_update/<int:pk>/',
+         CallResultUpdateView.as_view()), # обновляет статус заявки
+    path('api/v1/callslist/manager_update/<int:pk>/',
+         CallManagerUpdateView.as_view()), # обновляет менеджера в заявке
     path('catalog/', include('good.urls')),
     path('', include('commonpages.urls')),
     path('', include('blog.urls')),

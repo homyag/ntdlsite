792bdd66999f5f4239b7f167aea7280cc91192d6
Igor Antonov <homyaga@gmail.com>
2024-10-19 15:02:05 +0300

    добавлены уведомления оператору о запросе обратного звонка

--- commonpages/apps.py
+++ commonpages/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CommonpagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'commonpages'
+
+    def ready(self):
+        import commonpages.signals
---
+++ commonpages/signals.py
@@ -0,0 +1,20 @@
+from .models import CallbackRequest
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from leads.signals import send_telegram_message
+from leads.signals import admin_id
+from leads.signals import operator_id
+
+
+@receiver(post_save, sender=CallbackRequest)
+def notify_operator_on_callback_request(sender, instance, created, **kwargs):
+    if created:
+        message = (
+            f'Заявка с сайта tdleningrad.ru:\n'
+            f'<b>Имя</b>: {instance.name}\n'
+            f'<b>Телефон</b>: {instance.phone}\n'
+            f'<b>Дата создания</b>: {instance.created_at}'
+        )
+        send_telegram_message(operator_id, message)
